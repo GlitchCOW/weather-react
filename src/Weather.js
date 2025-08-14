b1595570f89f8d4729bc466b36bccee826dbfd4b
skye palm <skyerain11@gmail.com>
2025-08-14 10:43:58 -0600

    fixed the all issues

--- src/Weather.js
+++ src/Weather.js
@@ -11,14 +11,14 @@ export default function Weather(props){
   function handleResponse(response) {
     setWeatherData({
       ready: true,
-      coordinates: response.data.coord,
-      temperature: response.data.main.temp,
-      humidity: response.data.main.humidity,
-      date: new Date(response.data.dt * 1000),
-      description: response.data.weather[0].description,
-      icon: response.data.weather[0].icon,
+      coordinates: response.data.coordinates,
+      temperature: response.data.temperature.current,
+      humidity: response.data.temperature.humidity,
+      date: new Date(response.data.time * 1000),
+      description: response.data.condition.description,
+      icon: response.data.condition.icon,
       wind: response.data.wind.speed,
-      city: response.data.name,
+      city: response.data.city,
     });
   }
 
@@ -31,9 +31,9 @@ export default function Weather(props){
     setCity(event.target.value);
   }
 
- function search() {
-    const apiKey = "8d9838178b5b401f1b4e7cb5af18e210";
-    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+  function search() {
+    const apiKey = "c99a06af36400a3o817aacf43a5073bt"; 
+    let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}`;
     axios.get(apiUrl).then(handleResponse);
   }
 
@@ -69,4 +69,3 @@ export default function Weather(props){
     return "Loading...";
   }
 }
-  
--- src/WeatherForecast.js
+++ src/WeatherForecast.js
@@ -12,18 +12,20 @@ export default function WeatherForecast(props) {
   }, [props.coordinates]);
 
   function handleResponse(response) {
+    
     setForecast(response.data.daily);
     setLoaded(true);
   }
 
-   function load() {
-    let apiKey = "8d9838178b5b401f1b4e7cb5af18e210";
+  function load() {
+    let apiKey = "c99a06af36400a3o817aacf43a5073bt"; 
     let longitude = props.coordinates.lon;
     let latitude = props.coordinates.lat;
-    let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&appid=${apiKey}&units=metric`;
+    let apiUrl = `https://api.shecodes.io/weather/v1/forecast?lon=${longitude}&lat=${latitude}&key=${apiKey}`;
 
     axios.get(apiUrl).then(handleResponse);
   }
+
   if (loaded && forecast) {
     return (
       <div className="WeatherForecast">
